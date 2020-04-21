96dbf409c204ea5e125b82fd1d91c4dbe1d3386d
““此wch2512 <447867853@qq.com>
2020-04-21 20:27:42 +0800

    Initial commit

---
+++ src/com/try2/com/hello.java
@@ -0,0 +1,13 @@
+package com.try2.com;
+
+/**
+ * @author wch
+ * @date 2020/4/20 - 18:08
+ */
+public class hello {
+
+
+
+
+}
+
---
+++ src/com/try2/com/try2.java
@@ -0,0 +1,11 @@
+package com.try2.com;
+
+public class try2 {
+
+    public static void main(String[] args) {
+        System.out.println("helloworld!");
+    }
+
+
+
+}
